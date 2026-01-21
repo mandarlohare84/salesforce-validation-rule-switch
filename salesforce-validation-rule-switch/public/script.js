b3d1b80163aae99fadee1c0cdc1f4d132dc5600d
Mandar Lohare <mandarlohare73@gmail.com>
2026-01-22 00:38:58 +0530

    Update script.js

--- salesforce-validation-rule-switch/public/script.js
+++ salesforce-validation-rule-switch/public/script.js
@@ -1,17 +1,17 @@
 function login() {
   const clientId =
     "3MVG9HtWXcDGV.nHI0zRBlvfsubgRjN5dn43lAdTzUhA492G9g0Gu3X60EsDM0I3sEiSd3Gl.Kq.Ww3EGRvB2";
-  const redirectUri =
-  window.location.origin + "/oauth/callback";
+
+  const redirectUri = window.location.origin + "/oauth/callback";
+
   const url =
-    `https://login.salesforce.com/services/oauth2/authorize` +
-    `?response_type=code` +
-    `&client_id=${clientId}` +
-    `&redirect_uri=${redirectUri}`;
+    "https://login.salesforce.com/services/oauth2/authorize" +
+    "?response_type=code" +
+    "&client_id=" + clientId +
+    "&redirect_uri=" + encodeURIComponent(redirectUri);
 
   window.location.href = url;
 }
-
 let validationRules = [];
 
 async function getRules() {
